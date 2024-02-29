fec630e0853455fc55b125dd51c6dafc02689544
kate-fie <kate.k.fieseler@gmail.com>
2024-02-29 15:00:25 +0000

    Initial data push 🚚

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # EV-A71-2A-syndirella-run
+
+### Structures used for placement:
+1. Fragment hits: [fragments/clean_hits.sdf](fragments/clean_hits.sdf). 
+2. Template: [x0310_relaxed.pdb](fragments/x0310_relaxed.pdb). Created by Matteo Ferla using [relax.ipynb](https://github.com/matteoferla/EV-A71-2A-elaborations/blob/main/iteration-2/code/relax.ipynb).
---
+++ jobs/tar_things.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# For scripts required see: https://gist.github.com/matteoferla/e0496d5766c12a0ae1738b943b41a536
+# A few things don't work in CentOS 7 due to GNU lib C (glibc) 2.17, so it has to be run in a Singularity container
+
+#: << USAGE #########
+# source run_job.env
+# condor_submit /data/xchem-fragalysis/shared/target_script.condor
+#USAGE #########
+
+#: << INSTALLATION #############
+#export PIP_NO_USER=1; # gets ignored.
+#export PYTHONUSERBASE=$CONDA_PREFIX; # double tap
+#conda install -y -n base -c conda-forge openbabel plip;
+#pip install -q fragmenstein
+#pip install -q pyrosetta-help
+#PYROSETTA_USERNAME=levinthal PYROSETTA_PASSWORD=paradox install_pyrosetta
+#INSTALLATION ############
+
+export HOST=${HOST:-$(hostname)}
+export USER=${USER:-$(users)}
+export HOME=${HOME:-$_CONDOR_SCRATCH_DIR}
+source /etc/os-release;
+echo "Running script ${0} as $USER in $HOST which runs $PRETTY_NAME"
+# ---------------------------------------------------------------
+
+source /data/xchem-fragalysis/kfieseler/.bashrc
+
+cd /data/xchem-fragalysis/kfieseler/D68EV3CPROA/elabs
+
+pwd;
+tar -czvf batch_8 batch_9 batch_10 batch_11 batch_12 batch_13 batch_14 batch_8-14.tar.gz
+
+echo 'COMPLETE'
