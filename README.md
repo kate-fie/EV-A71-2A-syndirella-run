d3da87e4fd316ec63173a4b9fd445a16e2e9bc4f
kate-fie <kate.k.fieseler@gmail.com>
2024-03-04 21:07:24 +0000

    all batches scripts

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # EV-A71-2A-syndirella-run
 
 ### Structures used for placement:
-1. Fragment hits: [fragments/clean_hits.sdf](fragments/clean_hits.sdf). 
+1. Fragment hits: [fragments/new_hits.sdf](fragments/new_hits.sdf). (Had to download each individual .sdf from [Fragalysis](https://fragalysis.xchem.diamond.ac.uk/viewer/react/preview/target/A71EV2A/tas/lb32627-66)).
 2. Template: [x0310_relaxed_apo.pdb](fragments/x0310_relaxed_apo.pdb). Created by Matteo Ferla using [relax.ipynb](https://github.com/matteoferla/EV-A71-2A-elaborations/blob/main/iteration-2/code/relax.ipynb). 
 Then edited with [prepare_template](notebooks/prepare_template.ipynb).
 
 ### Status:
-**2024-03-04 13:30:** Running [batch0](batches/batch0.csv) again with relaxed apo template.
+**2024-03-04 13:30:** Running [batch0](batches/batch0.csv) again with relaxed apo template. Job 19760 on iris-gpu. 
 
 Currently running [batch0](batches/batch0.csv) as of 2024-02-29 19:00 with 9 base compounds and placing the top 10K.
 ➡️ Placements didn't work since template I was using still contained the ligand...
---
+++ jobs/run_batch1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# For scripts required see: https://gist.github.com/matteoferla/e0496d5766c12a0ae1738b943b41a536
+# A few things don't work in CentOS 7 due to GNU lib C (glibc) 2.17, so it has to be run in a Singularity container
+
+#: << USAGE #########
+# source run_job.env
+# condor_submit /data/xchem-fragalysis/shared/target_script.condor
+#USAGE #########
+
+#: << INSTALLATION #############
+#export PIP_NO_USER=1; # gets ignored.
+#export PYTHONUSERBASE=$CONDA_PREFIX; # double tap
+#conda install -y -n base -c conda-forge openbabel plip;
+#pip install -q fragmenstein
+#pip install -q pyrosetta-help
+#PYROSETTA_USERNAME=levinthal PYROSETTA_PASSWORD=paradox install_pyrosetta
+#INSTALLATION ############
+
+export HOST=${HOST:-$(hostname)}
+export USER=${USER:-$(users)}
+export HOME=${HOME:-$_CONDOR_SCRATCH_DIR}
+source /etc/os-release;
+echo "Running script ${0} as $USER in $HOST which runs $PRETTY_NAME"
+# ---------------------------------------------------------------
+
+source /data/xchem-fragalysis/kfieseler/.bashrc
+conda activate fragmenstein
+
+cd /data/xchem-fragalysis/kfieseler/syndirella
+
+pwd;
+export INPUT="/data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-run/batches/batch1.csv"
+export OUTPUT_DIR="/data/xchem-fragalysis/kfieseler/A71EV2A_run2/batch1";
+export TEMPLATE="/data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-run/fragments/x0310_relaxed_apo.pdb";
+export HITS="/data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-run/fragments/new_hits.sdf";
+export SYNDIRELLA_BASE_PATH="/data/xchem-fragalysis/kfieseler/syndirella";
+
+echo "Running syndirella pipeline"
+echo "INPUT: $INPUT"
+echo "OUTPUT_DIR: $OUTPUT_DIR"
+echo "TEMPLATE: $TEMPLATE"
+echo "HITS: $HITS"
+
+nice -19 python /data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-run/jobs/run_job.py \
+--input $INPUT \
+--output $OUTPUT_DIR \
+--template $TEMPLATE \
+--hits $HITS;
+
+echo 'COMPLETE'
