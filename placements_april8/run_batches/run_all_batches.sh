af974dcba68f474b7c0cec8d9bfb81d63a7022c9
kate-fie <kate.k.fieseler@gmail.com>
2024-04-08 16:54:05 +0100

    running new batches

--- placements_april8/run_batches/run_all_batches.sh
+++ placements_april8/run_batches/run_all_batches.sh
@@ -17,6 +17,7 @@ export JOB_INNER_SCRIPT="/data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-r
 
 for i in {0..1}; do
   export BATCH_NUM=$i
+  export JOB_INNER_SCRIPT="/data/xchem-fragalysis/kfieseler/EV-A71-2A-syndirella-run/placements_april8/run_batches/run_a_batch.sh"
   source /data/xchem-fragalysis/kfieseler/run_preset_job.env
   condor_submit /data/xchem-fragalysis/shared/target_script.condor
   sleep 1m
