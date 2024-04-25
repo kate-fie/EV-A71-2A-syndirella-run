3f0c023af34422c9b06519b30560be5b75d3aa0e
kate-fie <kate.k.fieseler@gmail.com>
2024-04-25 11:09:57 +0100

    running repeats

--- repeat_placements_april22/run.sh
+++ repeat_placements_april22/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=install_conda
+#SBATCH --job-name=repeats
 #SBATCH --chdir=/opt/xchem-fragalysis-2/kfieseler
 #SBATCH --output=/opt/xchem-fragalysis-2/kfieseler/logs/slurm-error_%x_%j.log
 #SBATCH --error=/opt/xchem-fragalysis-2/kfieseler/logs/slurm-error_%x_%j.log
